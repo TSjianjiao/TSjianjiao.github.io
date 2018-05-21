2ece1c1f03292624fc8e7ef4ba2928aa386ccea9
TSjianjiao <929175050@qq.com>
2018-05-21 15:53:33 +0800

    day28-30

--- day17-18/js/game_of_3.js
+++
@@ -1,9 +0,0 @@
-for (var i = 1; i<=100; i++) {
-    var message
-    if ((i % 3) === 0 || (i % 10) === 3 ) {
-        message = "PA";
-    }else {
-        message = i;
-    }
-    console.log(message + ",");
-}
