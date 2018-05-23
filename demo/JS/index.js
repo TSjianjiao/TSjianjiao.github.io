695c5da110f36c5b8ffb5ebde785ca71d102fb7a
TSjianjiao <929175050@qq.com>
2018-05-23 08:27:20 +0800

    demo

--- demo/JS/index.js
+++ demo/JS/index.js
@@ -1,5 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.douban.com/v2/book/1220562');
+xhr.open('GET', 'Access-Control-Allow-Origin:https://api.douban.com/v2/book/1220562');
 xhr.send(null);
 xhr.onload = function () {
     if (xhr.readyState === 4) {
