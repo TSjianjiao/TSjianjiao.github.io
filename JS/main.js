d78170f679830fe74132663cd1fe44bb98716ac8
TSjianjiao <929175050@qq.com>
2018-05-31 08:00:44 +0800

    1

--- JS/main.js
+++
@@ -1,7 +0,0 @@
-/**
- * @file 入口
- */
-var regionWrapper = document.getElementById('region-radio-wrapper');
-var productWrapper = document.getElementById('product-radio-wrapper');
-checkBoxEvent(regionWrapper);
-checkBoxEvent(productWrapper);
